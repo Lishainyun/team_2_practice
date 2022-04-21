d89895c9b787c2efa5697b57f3b382d58f0c7f15
nash15963 <nash1596315963@gmail.com>
2022-04-21 16:12:12 +0800

    tyson-002

--- js/tyson.js
+++ js/tyson.js
@@ -13,18 +13,19 @@ const ReportWeather =()=>{
         console.log(data.records.weatherElement.location[0].value)
         let location = ['嘉義','臺中','澎湖','宜蘭','金門']
         let tyson = document.querySelector('.tyson')
+        let newH3 = document.createElement('h3')
+        tyson.appendChild(newH3)
+        newH3.innerText = '天氣紫外線預測'
         let information = document.createElement('div')
         information.classList.add('information')
         for(let i=0 ; i<5 ;i++){
             let newLoc = document.createElement('p')
             newLoc.innerText = location[i]
-            
             let newP = document.createElement('p')
             newP.innerText = data.records.weatherElement.location[i].value
-            
             let newImg = document.createElement('img')
-            newImg.src = './img/weather.jpg'
-
+            // newImg.src = './img/weather.jpg'
+            newImg.src = 'https://nash15963.github.io/team_2_practice/img/weather.jpg'
             let newDiv = document.createElement('div')
             newDiv.classList.add('information')
             newDiv.appendChild(newLoc)
@@ -32,7 +33,6 @@ const ReportWeather =()=>{
             newDiv.appendChild(newImg)
             tyson.appendChild(newDiv)
         }
-
     })
 }
 ReportWeather()
